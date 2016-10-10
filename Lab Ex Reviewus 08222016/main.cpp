bd5afede05b798627e3456c2736603e0e5586090
alexberry721 <alexberry721@gmail.com>
2016-10-10 09:03:25 -0700

    Add aritmetic ops

--- Lab Ex Reviewus 08222016/main.cpp
+++ Lab Ex Reviewus 08222016/main.cpp
@@ -27,8 +27,25 @@ int main() {
 
 int main() {
     
-    // Goodbye World
-    cout << "Goodbye, World!\n";
+    // Declare memory storage we need
+    // We use double for floating point numbers to be stored for each of
+    // the memory locations named: sum, difference, product and quotient
+    // Initialize the memory locations to 0.0
+    
+    cout << "Aritmetic Operations\n";
+    
+    double sum = 0.0, difference = 0.0, product = 0.0, quotient = 0.0;
+    
+    sum = 30 + 4;
+    difference = 30 - 4;
+    product = 30 * 4;
+    quotient = 30 / 4;
+    
+    
+    cout << sum << endl;
+    cout << difference << endl;
+    cout << product << endl;
+    cout << quotient << endl;
     
     return 0;
 }
