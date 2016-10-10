15850a97190c83170ee80346285cfce1b14a2196
alexberry721 <alexberry721@gmail.com>
2016-10-10 08:49:06 -0700

    Hello world

--- Lab Ex Reviewus 08222016/main.cpp
+++ Lab Ex Reviewus 08222016/main.cpp
@@ -7,9 +7,12 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    // Hello World
+    cout << "Hello, World!\n";
+    
     return 0;
 }
